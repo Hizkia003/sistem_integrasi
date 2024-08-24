1e2a2173dec730d11de25a598b201ae0a1e1354f
Ruben <rubenhizkia03@gmail.com>
2024-08-24 19:03:38 +0700

    Membuat fitur penjumlahan dan pengurangan

--- fungsi.js
+++ fungsi.js
@@ -3,9 +3,3 @@ console.log(add(1,2));
 
 const subtract = (a, b) => a - b;
 console.log(subtract(1,3));
-
-const multiply = (a, b) => a * b;
-console.log(multiply(2,2));
-
-const divide = (a, b) => a / b;
-console.log(divide(6,2));

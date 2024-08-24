31a7a6267e19c0a854232c913e3eecb417467ead
Ruben <rubenhizkia03@gmail.com>
2024-08-24 14:42:29 +0700

    Menambah fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -5,7 +5,7 @@ const subtract = (a, b) => a - b;
 console.log(subtract(1,3));
 
 const multiply = (a, b) => a * b;
-console.log( multiply(2,2));
+console.log(multiply(2,2));
 
 const divide = (a, b) => a / b;
 console.log(divide(4,2));
